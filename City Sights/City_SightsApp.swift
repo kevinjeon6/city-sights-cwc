46ba554cc6b17c1b28a1356a290035082048c466
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-03-08 11:10:57 -0600

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by Kevin Mattocks on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
