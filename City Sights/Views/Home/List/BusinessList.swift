3f1c71f7894efeb6527917e9464c21ee24c7faa3
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-03-10 10:01:29 -0600

    Create BusinessList

---
+++ City Sights/Views/Home/List/BusinessList.swift
@@ -0,0 +1,20 @@
+//
+//  BusinessList.swift
+//  City Sights
+//
+//  Created by Kevin Mattocks on 3/10/22.
+//
+
+import SwiftUI
+
+struct BusinessList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BusinessList_Previews: PreviewProvider {
+    static var previews: some View {
+        BusinessList()
+    }
+}
